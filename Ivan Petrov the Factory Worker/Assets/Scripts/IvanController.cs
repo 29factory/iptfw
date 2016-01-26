3d99cdcb8ec640efcf5d7bcec78f5510de999a4f
ByPort <byport1112@gmail.com>
2016-01-26 16:14:25 +0300

    Initial project

---
+++ Ivan Petrov the Factory Worker/Assets/Scripts/IvanController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IvanController : MonoBehaviour {
+	public float speed = 16;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+	}
+}

d059e4213945dea6ad5f239a51566b3e25714f2c
ByPort <byport1112@gmail.com>
2016-04-01 20:39:19 +0300

    Интеграция остова меню
    Интеграция основных сцен меню, рефакторинг некоторых скриптов,
    интеграция остова системы сохранений

--- Ivan Petrov the Factory Worker/Assets/Scripts/IvanController.cs
+++ Ivan Petrov the Factory Worker/Assets/Scripts/IvanController.cs
@@ -4,8 +4,14 @@ using System.Collections;
 public class IvanController : MonoBehaviour {
 	public float speed = 20;
 
+    void Awake () {
+        GetComponent<Transform> ().position = GameObject.Find ("GameManagement").GetComponent<GameManagement> ().playOptions.appearAt;
+    }
+
 	void FixedUpdate () {
 		GetComponent<Rigidbody2D> ().velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
 		GetComponent<Animator> ().SetBool ("IsWalking", GetComponent<Rigidbody2D> ().velocity != new Vector2 (0f, 0f));
+        if (Input.GetButtonDown ("Cancel"))
+            GameObject.Find ("GameManagement").GetComponent<GameManagement> ().Save ();
 	}
 }
---
+++ Ivan Petrov the Factory Worker/Assets/Scripts/RegimeSelector.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RegimeSelector : MonoBehaviour {
+    public void Select(string r) {
+        GameObject.Find ("GameManagement").GetComponent<GameManagement> ().Play (r);
+    }
+}

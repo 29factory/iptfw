0f33c195d4fa11ffbc3915c88f602639b7792195
ByPort <byport1112@gmail.com>
2016-05-04 16:10:41 +0300

    Исправлена скорость передвижения по диагонали
    Теперь нормируется

--- Ivan Petrov the Factory Worker/Assets/Scripts/IvanController.cs
+++ Ivan Petrov the Factory Worker/Assets/Scripts/IvanController.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class IvanController : MonoBehaviour {
-	public float speed = 20;
+    public float speed;
 
 	void FixedUpdate () {
-		GetComponent<Rigidbody2D> ().velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+        GetComponent<Rigidbody2D> ().velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * speed;
 		GetComponent<Animator> ().SetBool ("IsWalking", GetComponent<Rigidbody2D> ().velocity != new Vector2 (0f, 0f));
 	}
 }

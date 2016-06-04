caa6fbb86969ea67b415f511a950668fc0658300
ByPort <byport1112@gmail.com>
2016-06-04 21:54:52 +0300

    Исправлена походка
    Иван теперь двигается и смотрит во все 4 стороны

--- Ivan Petrov the Factory Worker/Assets/Scripts/IvanController.cs
+++ Ivan Petrov the Factory Worker/Assets/Scripts/IvanController.cs
@@ -14,7 +14,17 @@ public class IvanController : MonoBehaviour {
 
 	void FixedUpdate () {
         GetComponent<Rigidbody2D> ().velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * speed;
-		GetComponent<Animator> ().SetBool ("IsWalking", GetComponent<Rigidbody2D> ().velocity != new Vector2 (0f, 0f));
+        GetComponent<Animator> ().SetBool ("IsWalking", GetComponent<Rigidbody2D> ().velocity != new Vector2 (0f, 0f));
+        if (GetComponent<Rigidbody2D> ().velocity != new Vector2 (0f, 0f)) {
+            if (GetComponent<Rigidbody2D> ().velocity.x > 0) GetComponent<Animator> ().SetFloat ("DirectionX", 1);
+            else if (GetComponent<Rigidbody2D> ().velocity.x < 0) GetComponent<Animator> ().SetFloat ("DirectionX", -1);
+            else GetComponent<Animator> ().SetFloat ("DirectionX", 0);
+            if (GetComponent<Rigidbody2D> ().velocity.y > 0) GetComponent<Animator> ().SetFloat ("DirectionY", 1);
+            else if (GetComponent<Rigidbody2D> ().velocity.y < 0) GetComponent<Animator> ().SetFloat ("DirectionY", -1);
+            else GetComponent<Animator> ().SetFloat ("DirectionY", 0);
+        }
+        GetComponent<Animator> ().SetFloat ("SpeedX", Input.GetAxis("Horizontal"));
+        GetComponent<Animator> ().SetFloat ("SpeedY", Input.GetAxis("Vertical"));
         if (Input.GetButtonDown ("Cancel")) {
             GameManagement.Save ((Location) System.Enum.Parse(typeof(Location), SceneManager.GetActiveScene().name), GetComponent<Transform> ().position);
             SceneManager.LoadScene ("Pause");
